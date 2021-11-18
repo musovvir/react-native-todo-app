f13c23ecb99b83abbc371d01b52232766b545875
musovvir <mr.muzagov@mail.ru>
2021-11-18 13:12:11 +0300

    add

--- App.js
+++ App.js
@@ -1,14 +1,54 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import { AddTodo } from "./src/AddTodo";
 import { Navbar } from "./src/Navbar";
+import { Todo } from "./src/Todo";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {id: 1, title: "Todos"},
+    {id: 2, title: "Todos"},
+    {id: 3, title: "Todos"},
+    {id: 4, title: "Todos"},
+    {id: 5, title: "Todos"},
+    {id: 6, title: "Todos"},
+    {id: 7, title: "Todos"},
+    {id: 8, title: "Todos"},
+    {id: 9, title: "Todos"},
+    {id: 10, title: "Todos"},
+    {id: 11, title: "Todos"},
+    {id: 12, title: "Todos"},
+    {id: 13, title: "Todos"},
+  ])
+
+  const addTodo = (title) => {
+    // const newTodo = {
+    //   id: Date.now().toString(),
+    //   title: title
+    // }
+
+    // setTodos((prevTodos) => {
+    //   return [
+    //     ...prevTodos,
+    //     newTodo
+    //   ]
+    // })
+
+    setTodos(prev => [...prev, {
+      id: Date.now().toString(),
+      title
+    }])
+  }
+
   return (
     <View>
       <Navbar title="Todo App" />
       <View style={styles.container}>
-        <AddTodo />
+        <AddTodo onSubmit={addTodo} />
+        <ScrollView>
+          {todos.map(todo => <Todo todo={todo} key={todo.id}/>
+          )}
+        </ScrollView>
       </View>
     </View>
   );
--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,11 +1,30 @@
-import React from "react";
-import { View, StyleSheet, TextInput, Button } from "react-native";
+import React, {useState} from "react";
+import { View, StyleSheet, TextInput, Button, Alert } from "react-native";
 
-export const AddTodo = () => {
+export const AddTodo = ({onSubmit}) => {
+  const [value, setValue] = useState('')
+
+  const pressHandler = () => {
+    if(value.trim()) {
+      onSubmit(value)
+      setValue('')
+    } else {
+      Alert.alert('Название дела не может быть пустым')
+    }
+    
+  }
+
   return (
     <View style={styles.block}>
-      <TextInput style={styles.input} />
-      <Button title="Добавить" />
+      <TextInput
+      style={styles.input}
+      onChangeText={setValue}
+      value={value}
+      placeholder='Введите название дела...'
+      autoCorrect={false}
+      autoCapitalize='none'
+      />
+      <Button title="Добавить" onPress={pressHandler}/>
     </View>
   );
 };
@@ -14,7 +33,8 @@ const styles = StyleSheet.create({
   block: {
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: "center"
+    alignItems: "center",
+    marginBottom: 15
   },
   input: {
     width: "70%",
